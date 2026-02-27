05185b9b46d92929c858bce3f77574dee40f0178
BlessingMbiya <2425555@brunel.ac.uk>
2026-02-27 12:53:00 +0000

    Backend

---
+++ Labs/Week 20 Lab/Backend/src/test/java/a14startup2/tspage/TspageApplicationTests.java
@@ -0,0 +1,13 @@
+package a14startup2.tspage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TspageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
